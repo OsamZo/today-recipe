2424085a91fe05a46c40c39f6ff2d18ef6ea2054
kookong2 <hy010309@naver.com>
2024-10-29 10:00:59 +0900

    [FEAT] 스프링 프로젝트 추가 작업

---
+++ spring/src/test/java/com/today_recipe/spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.today_recipe.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
