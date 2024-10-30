baf866c05bde3e2c59a3327ecfa8645ec2b183a1
kookong2 <hy010309@naver.com>
2024-10-30 18:13:04 +0900

    [REFACTOR] SecurityConfig 변경

--- spring/src/main/java/com/goruna/spring/security/SecurityConfig.java
+++ spring/src/main/java/com/goruna/spring/security/SecurityConfig.java
@@ -12,12 +12,13 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        // 인증 무효화 및 특정 경로에 대한 접근 허용 설정
+
         http
-                .authorizeRequests(authorize -> authorize
+                .authorizeHttpRequests(authorize -> authorize
                         .anyRequest().permitAll() // 모든 요청에 대한 인증 비활성화
-                )
-                .csrf().disable(); // CSRF 보호 비활성화 (필요에 따라 활성화 가능)
+                );
+
+        http.csrf(csrf -> csrf.disable());
 
         return http.build();
     }
