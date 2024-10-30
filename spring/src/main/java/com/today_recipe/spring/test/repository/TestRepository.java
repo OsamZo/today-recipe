ded4e4c1846792b6fee50385899e40bf7c294e65
kookong2 <hy010309@naver.com>
2024-10-30 18:11:57 +0900

    [CHORE] 파일 수정으로 인한 추가된 파일 삭제

--- spring/src/main/java/com/today_recipe/spring/test/repository/TestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.today_recipe.spring.test.repository;
-
-import com.today_recipe.spring.test.entity.Test;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestRepository extends JpaRepository<Test, Long> {
-}
