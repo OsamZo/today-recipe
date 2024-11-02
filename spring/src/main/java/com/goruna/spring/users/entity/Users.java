c5011f1c81c3c13a65158dd0300a0ba55273fd0d
wildcat222 <wildcat222@naver.com>
2024-11-02 22:13:00 +0900

    feat: Users Entity 생성

---
+++ spring/src/main/java/com/goruna/spring/users/entity/Users.java
@@ -0,0 +1,42 @@
+package com.goruna.spring.users.entity;
+
+import com.goruna.spring.common.aggregate.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@Getter
+public class Users extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_seq", nullable = false)
+    private long userSeq;
+
+    @Column(name = "user_email", nullable = false)
+    private String userEmail;
+
+    @Column(name = "user_nickname", nullable = false)
+    private String userNickname;
+
+    @Column(name = "user_status", nullable = false)
+    private String userStatus = "active";
+
+    @Column(name = "user_role", nullable = false)
+    private UserRole userRole = UserRole.USER;
+
+    @Column(name = "user_deleted_time", nullable = true)
+    private LocalDateTime userDeletedTime;
+
+    @Column(name = "user_saved_money", nullable = false)
+    private int userSavedMoney;
+
+    @Column(name = "user_usage_count", nullable = false)
+    private int userUsageCount = 0;
+
+}
