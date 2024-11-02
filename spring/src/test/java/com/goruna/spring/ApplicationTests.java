6a2e83a9b5402999a2bc01d83b9e97c873281eaa
kookong2 <hy010309@naver.com>
2024-11-02 22:07:39 +0900

    [RENAME] 파일 변경으로 인한 코드 수정

--- spring/src/test/java/com/goruna/spring/ApplicationTests.java
+++ spring/src/test/java/com/goruna/spring/ApplicationTests.java
@@ -1,13 +1,14 @@
-package com.today_recipe.spring;
+package com.goruna.spring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")  // 테스트용 프로파일 설정
 class ApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
