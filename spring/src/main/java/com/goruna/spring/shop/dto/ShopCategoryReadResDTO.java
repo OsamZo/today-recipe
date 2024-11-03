ed14c7148132302cd4e25e0abed8c5c76d226d73
hyomee2 <s5763305@naver.com>
2024-11-03 23:41:58 +0900

    [FEAT] 매장 카테고리 조회 응답 DTO 생성

---
+++ spring/src/main/java/com/goruna/spring/shop/dto/ShopCategoryReadResDTO.java
@@ -0,0 +1,15 @@
+package com.goruna.spring.shop.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class ShopCategoryReadResDTO {
+
+    private Long categorySeq;
+
+    private String categoryName;
+}
