fba04eea706703703d8484dd4e4749d8649e1a17
kookong2 <hy010309@naver.com>
2024-11-03 12:28:19 +0900

    [FEAT] 좋아요 엔티티 일대다 매핑

--- spring/src/main/java/com/goruna/spring/users/entity/User.java
+++ spring/src/main/java/com/goruna/spring/users/entity/User.java
@@ -2,11 +2,13 @@ package com.goruna.spring.users.entity;
 
 import com.goruna.spring.common.aggregate.UserRole;
 import com.goruna.spring.common.aggregate.entity.BaseTimeEntity;
+import com.goruna.spring.review.entity.Good;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -41,4 +43,6 @@ public class User extends BaseTimeEntity {
     @Column(name = "user_usage_count", nullable = false)
     private int userUsageCount = 0;
 
+    @OneToMany(mappedBy = "user")
+    private List<Good> goods;
 }
