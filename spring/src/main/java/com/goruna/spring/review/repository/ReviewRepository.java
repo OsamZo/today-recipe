abb1654168bb1cd36144d88d94738c2670753c3c
kookong2 <hy010309@naver.com>
2024-11-04 01:46:37 +0900

    [FEAT] ReviewRepository 추가

---
+++ spring/src/main/java/com/goruna/spring/review/repository/ReviewRepository.java
@@ -0,0 +1,8 @@
+package com.goruna.spring.review.repository;
+
+import com.goruna.spring.review.entity.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+}
