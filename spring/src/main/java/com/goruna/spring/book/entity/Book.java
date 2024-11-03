0b79c36954b9cc8155cb68b9ce409aa68c049334
WhooGeek <rlarjswn0303@naver.com>
2024-11-03 17:52:48 +0900

    [FEAT] 예약 엔티티 생성

---
+++ spring/src/main/java/com/goruna/spring/book/entity/Book.java
@@ -0,0 +1,38 @@
+package com.goruna.spring.book.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "shop")
+@NoArgsConstructor
+@Getter
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "book_seq", nullable = false)
+    private long bookSeq;
+
+    @Column(name = "shop_seq", nullable = false)
+    private long shopSeq;
+
+    @Column(name = "user_seq", nullable = false)
+    private long userSeq;
+
+    @Column(name = "book_qty", nullable = false)
+    private int bookQty;
+
+    @Column(name = "book_receipt_status", nullable = false)
+    private boolean bookReceiptStatus = false;
+
+    @Column(name = "product_original_price", nullable = false)
+    private int productOriginalPrice;
+
+    @Column(name = "product_sale_price", nullable = false)
+    private int productSalePrice;
+
+    @Column(name = "book_status", nullable = false)
+    private boolean bookStatus;
+}
