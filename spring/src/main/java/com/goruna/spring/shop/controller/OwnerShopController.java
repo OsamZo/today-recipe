563074942aed5344785e733009f0f566caad38a2
wildcat222 <wildcat222@naver.com>
2024-11-04 12:33:51 +0900

    feat: controller (사장) 매장 정보 수정 기능 구현

--- spring/src/main/java/com/goruna/spring/shop/controller/OwnerShopController.java
+++ spring/src/main/java/com/goruna/spring/shop/controller/OwnerShopController.java
@@ -1,10 +1,13 @@
 package com.goruna.spring.shop.controller;
 
+import com.goruna.spring.common.response.ApiResponse;
+import com.goruna.spring.common.response.ResponseUtil;
+import com.goruna.spring.shop.dto.UpdateShopInfoDTO;
 import com.goruna.spring.shop.service.OwnerShopService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/owner")
@@ -13,5 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class OwnerShopController {
 
     private final OwnerShopService ownerShopService;
+
+    @PutMapping("/shop/{shopSeq}")
+    @Operation(summary = "(사장) 매장 정보 수정", description = "(사장) 매장 정보를 수정합니다.")
+    public ApiResponse<?> updateShopInfo(@PathVariable Long shopSeq, @RequestBody UpdateShopInfoDTO updateShopInfoDTO) {
+        ownerShopService.updateShopInfo(shopSeq, updateShopInfoDTO);
+        return ResponseUtil.successResponse("데이터가 성공적으로 수정되었습니다").getBody();
+    }
 
 }
