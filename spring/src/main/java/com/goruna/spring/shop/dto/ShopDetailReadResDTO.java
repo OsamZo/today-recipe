e3788a72209ae1efc00a0077681a23ed6536e825
hyomee2 <s5763305@naver.com>
2024-11-04 16:41:03 +0900

    [FEAT] 매장 상세 조회 기능 구현

---
+++ spring/src/main/java/com/goruna/spring/shop/dto/ShopDetailReadResDTO.java
@@ -0,0 +1,40 @@
+package com.goruna.spring.shop.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalTime;
+
+@Getter
+@Setter
+@ToString
+public class ShopDetailReadResDTO {
+
+    private Long shopSeq;
+
+    private String shopName;
+
+    private LocalTime shopClosedAt;
+
+    private String shopAddress;
+
+    private String shopIntroduction;
+
+    private String shopTodayComment;
+
+    private String shopProductName;
+
+    private int shopProductQty;
+
+    private int shopProductOriginalPrice;
+
+    private int shopProductSalePrice;
+
+    private String shopProductDesc;
+
+    private String shopImgUrl;
+
+    private String shopProductImgUrl;
+}
+
