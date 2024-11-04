8c88cb4b7bcafb137b74a6d2ff81004188ead685
kookong2 <hy010309@naver.com>
2024-11-04 15:28:25 +0900

    [FEAT] BookmarkRepository 추가

---
+++ spring/src/main/java/com/goruna/spring/bookmark/repository/BookmarkRepository.java
@@ -0,0 +1,8 @@
+package com.goruna.spring.bookmark.repository;
+
+import com.goruna.spring.bookmark.entity.Bookmark;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
+
+}
