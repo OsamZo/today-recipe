5fd8ce1761cb0809d5175e8c9c25bdee8e860bbf
hyomee2 <s5763305@naver.com>
2024-11-04 15:19:29 +0900

    [CHORE] @Tag 추가

--- spring/src/main/java/com/goruna/spring/shop/controller/ShopController.java
+++ spring/src/main/java/com/goruna/spring/shop/controller/ShopController.java
@@ -5,6 +5,7 @@ import com.goruna.spring.common.response.ResponseUtil;
 import com.goruna.spring.shop.dto.ShopListReadResDTO;
 import com.goruna.spring.shop.service.ShopService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Tag(name = "shop", description="매장 API")
 @RestController
 @RequiredArgsConstructor
 public class ShopController {
