89ccb0129b56526a4cca9da9a735e9785f289bcc
WhooGeek <rlarjswn0303@naver.com>
2024-11-04 15:35:20 +0900

    [REFACTOR] AdminAuthShopController 페이징 처리 추가

--- spring/src/main/java/com/goruna/spring/shop/controller/AdminAuthShopController.java
+++ spring/src/main/java/com/goruna/spring/shop/controller/AdminAuthShopController.java
@@ -9,9 +9,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,8 +23,11 @@ public class AdminAuthShopController {
 
     @GetMapping
     @Operation(summary = "매장 등록 인증 전체 조회", description = "매장 데이터 전체를 조회합니다.")
-    public ApiResponse<?> getAdminAuthAllShop(){
-        List<AdminAuthShopResponseDTO> allAuthShops = adminAuthSearchService.getAdminAuthAllShop();
+    public ApiResponse<?> getAdminAuthAllShop(
+            @RequestParam(defaultValue = "1") Integer page,
+            @RequestParam(defaultValue = "10") Integer size
+    ){
+        List<AdminAuthShopResponseDTO> allAuthShops = adminAuthSearchService.getAdminAuthAllShop(page, size);
         return ResponseUtil.successResponse("전체 매장 등록 인증 데이터가 성공적으로 조회되었습니다.", allAuthShops).getBody();
     }
 
