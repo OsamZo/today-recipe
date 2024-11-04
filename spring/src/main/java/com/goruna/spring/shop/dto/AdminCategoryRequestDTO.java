bcef53081429e2dc341910e69d0a4cc1deefee6e
WhooGeek <rlarjswn0303@naver.com>
2024-11-04 16:39:04 +0900

    [FEAT] 카테고리 추가 기능 AdminCategoryRequestDTO 추가 #80

---
+++ spring/src/main/java/com/goruna/spring/shop/dto/AdminCategoryRequestDTO.java
@@ -0,0 +1,17 @@
+package com.goruna.spring.shop.dto;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AdminCategoryRequestDTO {
+
+    private String categoryName;
+
+}
