0fe1c333d50fdc83c21b113ebc0c5144901642a1
WhooGeek <rlarjswn0303@naver.com>
2024-11-05 16:39:52 +0900

    [FEAT] #126 관리자 매장 검색 기능 구현 AdminShopService 코드

--- spring/src/main/java/com/goruna/spring/shop/service/AdminShopService.java
+++ spring/src/main/java/com/goruna/spring/shop/service/AdminShopService.java
@@ -3,8 +3,10 @@ package com.goruna.spring.shop.service;
 import com.goruna.spring.common.exception.CustomException;
 import com.goruna.spring.common.exception.ErrorCodeType;
 import com.goruna.spring.shop.dto.AdminShopResponseDTO;
+import com.goruna.spring.shop.dto.AdminShopSearchResponseDTO;
 import com.goruna.spring.shop.entity.Shop;
 import com.goruna.spring.shop.repository.AdminShopRepository;
+import com.goruna.spring.shop.repository.ShopRepository;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
@@ -20,7 +22,7 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class AdminShopService {
 
-    private final AdminShopRepository adminSearchRepository;
+    private final ShopRepository shopRepository;
     private final ModelMapper modelMapper;
 
     // 매장 데이터 전체 조회
@@ -34,11 +36,29 @@ public class AdminShopService {
         int offset = (page - 1) * size;
 
         Pageable pageable = PageRequest.of(offset, size);
-        Page<Shop> shopsPage = adminSearchRepository.findAll(pageable);
+        Page<Shop> shopsPage = shopRepository.findAll(pageable);
         List<Shop> Shops = shopsPage.getContent();
 
         return Shops.stream()
                 .map(shop -> modelMapper.map(shop, AdminShopResponseDTO.class))
                 .collect(Collectors.toList());
+    }
+
+    // 매장 데이터 검색
+    @Transactional
+    public List<AdminShopSearchResponseDTO> getAdminShop(String shopName, Integer page, Integer size) {
+
+        if(page < 1 || size < 1) {
+            throw new CustomException(ErrorCodeType.INVALID_VALUE);
+        }
+        int offset = (page - 1) * size;
+        Pageable pageable = PageRequest.of(offset, size);
+        Page<Shop> shopsPage = shopRepository.findByShopNameContaining(pageable, shopName);
+        List<Shop> Shops = shopsPage.getContent();
+
+        return Shops.stream()
+                .map(shop -> modelMapper.map(shop, AdminShopSearchResponseDTO.class))
+                .collect(Collectors.toList());
+
     }
 }
