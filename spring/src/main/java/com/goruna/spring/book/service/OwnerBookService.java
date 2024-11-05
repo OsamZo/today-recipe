45320a54025f0cba0a0dd9f164a8b0bb738bde13
wildcat222 <wildcat222@naver.com>
2024-11-05 23:51:28 +0900

    chore: 주석, 확인용 코드 삭제 및 토큰에서 추출한 값 사용하도록 적용

--- spring/src/main/java/com/goruna/spring/book/service/OwnerBookService.java
+++ spring/src/main/java/com/goruna/spring/book/service/OwnerBookService.java
@@ -3,21 +3,17 @@ package com.goruna.spring.book.service;
 import com.goruna.spring.book.dto.OwnerShopBooksResDTO;
 import com.goruna.spring.book.entity.QBook;
 import com.goruna.spring.book.repository.BookRepository;
-import com.goruna.spring.common.exception.CustomException;
-import com.goruna.spring.common.exception.ErrorCodeType;
+import com.goruna.spring.common.util.CustomUserUtils;
 import com.goruna.spring.product.entity.QProduct;
 import com.goruna.spring.shop.entity.QShop;
 import com.goruna.spring.shop.repository.OwnerShopRepository;
 import com.goruna.spring.users.entity.QUser;
 import com.querydsl.core.types.Projections;
-import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
-import static java.lang.Integer.sum;
 
 @Service
 @RequiredArgsConstructor
@@ -29,9 +25,7 @@ public class OwnerBookService {
 
     public List<OwnerShopBooksResDTO> getOwnerShopBooks() {
 
-        Long userSeq = 1L;
-//        Long shopSeq = ownerShopRepository.findById(userSeq)
-//                .orElseThrow(() -> new CustomException(ErrorCodeType.SHOP_NOT_FOUND)).getShopSeq();
+        Long userSeq = CustomUserUtils.getCurrentUserSeq();   // 토큰에서 추출
 
         QUser user = QUser.user;
         QShop shop = QShop.shop;
@@ -47,10 +41,6 @@ public class OwnerBookService {
                         book.bookIsProductReceived
                         ))
                 .from(user)
-//                .join(shop).on(user.userSeq.eq(shop.user.userSeq))
-//                .join(product).on(shop.shopSeq.eq(product.shop.shopSeq))
-//                .join(book).on(product.productSeq.eq(book.bookSeq))
-//                .where(shop.user.userSeq.eq(userSeq))
                 .join(user).on(shop.user.userSeq.eq(user.userSeq)) // 사용자와 매장 조인
                 .join(product).on(shop.shopSeq.eq(product.shop.shopSeq)) // 매장과 제품 조인
                 .join(book).on(product.productSeq.eq(book.product.productSeq)) // 제품과 예약 조인
