a2523bce9cbbab4106d4430d60755edc5f0fe4e4
kookong2 <hy010309@naver.com>
2024-11-05 20:14:42 +0900

    [FEAT] BookmarkCreatResDTO 추가

---
+++ spring/src/main/java/com/goruna/spring/bookmark/dto/BookmarkCreatResDTO.java
@@ -0,0 +1,13 @@
+package com.goruna.spring.bookmark.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class BookmarkCreatResDTO {
+
+    private Long BookmarkSeq;
+}
