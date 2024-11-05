bc73d6631a4774e9903219ac0881e0a107ebce86
kookong2 <hy010309@naver.com>
2024-11-05 16:03:26 +0900

    [FEAT] ReviewController 리뷰 조회 API 구현

--- spring/src/main/java/com/goruna/spring/review/controller/ReviewController.java
+++ spring/src/main/java/com/goruna/spring/review/controller/ReviewController.java
@@ -3,11 +3,15 @@ package com.goruna.spring.review.controller;
 import com.goruna.spring.common.response.ApiResponse;
 import com.goruna.spring.common.response.ResponseUtil;
 import com.goruna.spring.review.dto.ReviewCreateRequestDTO;
+import com.goruna.spring.review.dto.ReviewShopReadResDTO;
+import com.goruna.spring.review.dto.ReviewUserReadResDTO;
 import com.goruna.spring.review.service.ReviewService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -27,6 +31,21 @@ public class ReviewController {
 
         reviewService.createReview(bookSeq, reviewCreateRequestDTO);
         return ResponseUtil.successResponse("리뷰가 성공적으로 등록되었습니다.").getBody();
+    }
+
+    /* 내가 작성한 리뷰 조회 API */
+    @Operation(summary = "내가 작성한 리뷰 조회 API", description = "사용자가 작성한 리뷰 조회 API입니다.")
+    @GetMapping("/user/{userSeq}/review")
+    public ApiResponse<?> getUserReviews(@PathVariable Long userSeq) {
+        List<ReviewUserReadResDTO> reviews = reviewService.getUserReviews(userSeq);
+        return ResponseUtil.successResponse("리뷰를 성공적으로 조회했습니다.", reviews).getBody();
+    }
+
+    @Operation(summary = "매장 리뷰 조회 API", description = "특정 매장의 상품에 대한 리뷰를 조회하는 API입니다.")
+    @GetMapping("/shop/{shopSeq}/review")
+    public ApiResponse<?> getShopReviews(@PathVariable Long shopSeq) {
+        List<ReviewShopReadResDTO> reviews = reviewService.getReviewsByShopId(shopSeq);
+        return ResponseUtil.successResponse("리뷰를 성공적으로 조회했습니다.", reviews).getBody();
     }
 
 
