ebea996a381bbbfb8f348e83877f79798896c40b
minju <kminju0224@gmail.com>
2024-11-05 15:14:02 +0900

    feat: JwtUtil 설정

---
+++ spring/src/main/java/com/goruna/spring/security/filter/JwtFilter.java
@@ -0,0 +1,42 @@
+package com.goruna.spring.security.filter;
+
+import com.goruna.spring.security.util.JwtUtil;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        log.info("JwtFilter activated");
+        String authorizationHeader = request.getHeader("Authorization");
+        log.info("Authorization header: {}", authorizationHeader);
+
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            String token = authorizationHeader.substring(7);
+            log.info("Token: {}", token);
+
+            if(jwtUtil.validateToken(token)) {
+                Long userSeq = jwtUtil.getUserSeq(token);
+                log.info("UserSeq from: {}", userSeq);
+                Authentication authentication = jwtUtil.getAuthentication(token);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
