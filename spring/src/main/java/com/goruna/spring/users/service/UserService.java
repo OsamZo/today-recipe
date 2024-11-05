2577950402568d6a26d2260519a2203759f9afbe
minju <kminju0224@gmail.com>
2024-11-05 15:20:05 +0900

    chore: 코드 정리

--- spring/src/main/java/com/goruna/spring/users/service/UserService.java
+++ spring/src/main/java/com/goruna/spring/users/service/UserService.java
@@ -1,27 +1,18 @@
 package com.goruna.spring.users.service;
 
+import com.goruna.spring.security.util.JwtUtil;
 import com.goruna.spring.users.dto.SignUpRequestDTO;
 
 import com.goruna.spring.users.entity.User;
 import com.goruna.spring.users.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-
-import java.util.ArrayList;
-import java.util.List;
-
-
 @Service
 @RequiredArgsConstructor
-public class UserService implements UserDetailsService{
+public class UserService{
 
     private final UserRepository userRepository;
     private final ModelMapper modelMapper;
@@ -31,18 +22,6 @@ public class UserService implements UserDetailsService{
         User user = modelMapper.map(signUpRequestDTO, User.class);
         userRepository.save(user);
         return signUpRequestDTO;
-    }
-
-    @Override
-    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
-
-        User loginUser = userRepository.findByUserEmail(userEmail)
-                .orElseThrow(() -> new UsernameNotFoundException(userEmail));
-
-        System.out.println(loginUser);
-        List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
-        grantedAuthorities.add(new SimpleGrantedAuthority(loginUser.getUserRole().name()));
-        return new org.springframework.security.core.userdetails.User(loginUser.getUserEmail(), "{noop}1", grantedAuthorities);
     }
 
 }
