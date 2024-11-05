9b6a4e5f35741cc96a64381c5a2ce72024a2de17
wildcat222 <wildcat222@naver.com>
2024-11-05 17:19:12 +0900

    feat: 상품 repository 생성

---
+++ spring/src/main/java/com/goruna/spring/product/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.goruna.spring.product.repository;
+
+import com.goruna.spring.product.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
