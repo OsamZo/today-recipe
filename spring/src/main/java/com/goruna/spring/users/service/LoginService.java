6fa3f86b1d5e20de8dd29e4ab9e076f4145fb337
minju <kminju0224@gmail.com>
2024-11-06 00:30:31 +0900

    [FEAT] 로그인 service 구현

--- spring/src/main/java/com/goruna/spring/users/service/LoginService.java
+++ spring/src/main/java/com/goruna/spring/users/service/LoginService.java
@@ -1,7 +1,7 @@
 package com.goruna.spring.users.service;
 
-import com.goruna.spring.users.dto.SignUpRequestDTO;
-
+import com.goruna.spring.security.util.JwtUtil;
+import com.goruna.spring.users.dto.UserRequestDto;
 import com.goruna.spring.users.entity.User;
 import com.goruna.spring.users.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -9,18 +9,31 @@ import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-@Service
+import java.util.Optional;
+
 @RequiredArgsConstructor
-public class UserService{
+@Service
+public class LoginService {
 
     private final UserRepository userRepository;
     private final ModelMapper modelMapper;
+    private final JwtUtil jwtUtil;
 
     @Transactional
-    public SignUpRequestDTO createUser(SignUpRequestDTO signUpRequestDTO) {
-        User user = modelMapper.map(signUpRequestDTO, User.class);
-        userRepository.save(user);
-        return signUpRequestDTO;
-    }
+    public String socialLogin(String userEmail) {
+        Optional<User> user = userRepository.findByUserEmail(userEmail);
 
+        if (!user.isPresent()) {
+            UserRequestDto userRequestDto = new UserRequestDto();
+            userRequestDto.setUserEmail(userEmail);
+            userRequestDto.setUserNickname("unKnown");
+
+            User newUser = modelMapper.map(userRequestDto, User.class);
+            userRepository.save(newUser);
+            return "회원가입 완료: " + userEmail;
+        } else {
+            String token = jwtUtil.generateToken(user.get().getUserSeq(), userEmail);
+            return "로그인 성공: " + userEmail + ", Token: " + token;
+        }
+    }
 }
