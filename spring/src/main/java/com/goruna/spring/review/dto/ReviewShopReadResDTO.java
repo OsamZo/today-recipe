1c9b9d56ce70ce3623b016e8cb5fa92fef6b771c
kookong2 <hy010309@naver.com>
2024-11-05 15:49:51 +0900

    [FEAT] ReviewShopReadResDTO 추가

---
+++ spring/src/main/java/com/goruna/spring/review/dto/ReviewShopReadResDTO.java
@@ -0,0 +1,18 @@
+package com.goruna.spring.review.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+public class ReviewShopReadResDTO {
+
+    private String shopImgUrl;
+    private String shopName;
+    private String userNickname;
+    private LocalDateTime regDate;
+    private String reviewContent;
+    private int likeCount;
+}
