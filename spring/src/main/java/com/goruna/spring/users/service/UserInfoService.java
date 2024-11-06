d658eec78f3cf8a54bf50ce731825852c4012387
minju <kminju0224@gmail.com>
2024-11-06 14:49:05 +0900

    [FEAT] 회원 정보 조회 service 구현

--- spring/src/main/java/com/goruna/spring/users/service/UserInfoService.java
+++ spring/src/main/java/com/goruna/spring/users/service/UserInfoService.java
@@ -2,6 +2,7 @@ package com.goruna.spring.users.service;
 
 import com.goruna.spring.common.util.CustomUserUtils;
 import com.goruna.spring.users.dto.NickNameRequestDto;
+import com.goruna.spring.users.dto.UserInfoResponse;
 import com.goruna.spring.users.entity.User;
 import com.goruna.spring.users.repository.UserRepository;
 import jakarta.persistence.EntityNotFoundException;
@@ -18,10 +19,15 @@ public class UserInfoService {
     private final UserRepository userRepository;
 
     @Transactional
-    public void createNickname(Long userSeq, NickNameRequestDto nickNameRequestDto) {
+    public void createNickname(NickNameRequestDto nickNameRequestDto) {
         User user = userRepository.findById(CustomUserUtils.getCurrentUserSeq())
                 .orElseThrow(EntityNotFoundException::new);
         modelMapper.map(nickNameRequestDto, user);
         userRepository.save(user);
+    }
+
+    public UserInfoResponse getUserInfo(Long userSeq) {
+        User user = userRepository.getUserInfo(CustomUserUtils.getCurrentUserSeq());
+        return modelMapper.map(user, UserInfoResponse.class);
     }
 }
