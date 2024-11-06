abd3efca543e52163ff83c01d41721ed5f765d11
wildcat222 <wildcat222@naver.com>
2024-11-06 15:20:21 +0900

    feat: remote pull 과정 중 충돌 수정

--- spring/src/main/java/com/goruna/spring/book/controller/OwnerBookController.java
+++ spring/src/main/java/com/goruna/spring/book/controller/OwnerBookController.java
@@ -28,6 +28,7 @@ public class OwnerBookController {
     public ApiResponse<?> updateBookInfo(@PathVariable Long bookSeq, @RequestBody UpdateBookInfoReqDTO updateBookInfoReqDTO) {
         ownerBookService.updateBookInfo(bookSeq, updateBookInfoReqDTO);
         return ResponseUtil.successResponse("예약 정보가 성공적으로 수정되었습니다.").getBody();
+    }
 
     @GetMapping("/book")
     @Operation(summary = "(사장) 내 매장 예약 목록 조회", description = "(사장) 내 매장 예약 목록을 조회합니다.")
--- spring/src/main/java/com/goruna/spring/book/service/OwnerBookService.java
+++ spring/src/main/java/com/goruna/spring/book/service/OwnerBookService.java
@@ -1,37 +1,33 @@
 package com.goruna.spring.book.service;
 
-import com.goruna.spring.book.dto.UpdateBookInfoReqDTO;
 import com.goruna.spring.book.dto.OwnerShopBooksResDTO;
+import com.goruna.spring.book.dto.UpdateBookInfoReqDTO;
 import com.goruna.spring.book.entity.Book;
 import com.goruna.spring.book.repository.BookRepository;
 import com.goruna.spring.common.exception.CustomException;
 import com.goruna.spring.common.exception.ErrorCodeType;
 import com.goruna.spring.common.util.CustomUserUtils;
 import lombok.RequiredArgsConstructor;
-import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class OwnerBookService {
 
     private final BookRepository bookRepository;
-    private final ModelMapper modelMapper;
 
     public void updateBookInfo(Long bookSeq, UpdateBookInfoReqDTO updateBookInfoReqDTO) {
         Book book = bookRepository.findById(bookSeq)
                 .orElseThrow(() -> new CustomException(ErrorCodeType.DATA_NOT_FOUND));
+    }
 
     public List<OwnerShopBooksResDTO> getOwnerShopBooks() {
 
-//        Long userSeq = CustomUserUtils.getCurrentUserSeq();   // 토큰에서 추출
-        Long userSeq = 3L;
+        Long userSeq = CustomUserUtils.getCurrentUserSeq();   // 토큰에서 추출
         List<OwnerShopBooksResDTO> books = bookRepository.findOwnerShopBooksByUserSeq(userSeq);
 
-        modelMapper.map(updateBookInfoReqDTO, book);
         return books;
     }
 }
