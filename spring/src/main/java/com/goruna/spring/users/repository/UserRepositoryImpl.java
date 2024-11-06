dbd0feae70957c927b1ae565e9f91c8b84310039
minju <kminju0224@gmail.com>
2024-11-06 18:24:29 +0900

    [FEAT] 회원 정보 수정 jpaQuery

--- spring/src/main/java/com/goruna/spring/users/repository/UserRepositoryImpl.java
+++ spring/src/main/java/com/goruna/spring/users/repository/UserRepositoryImpl.java
@@ -1,5 +1,6 @@
 package com.goruna.spring.users.repository;
 
+import com.goruna.spring.users.dto.UserInfoRequestDto;
 import com.goruna.spring.users.entity.User;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
@@ -21,4 +22,18 @@ public class UserRepositoryImpl implements UserRepositoryCustom{
                 .where(user.userSeq.eq(currentUserSeq))
                 .fetchOne();
     }
+
+    @Override
+    public User getUserNickName(Long currentUserSeq, UserInfoRequestDto userInfoRequestDto) {
+
+        jpaQueryFactory.update(user)
+                .where(user.userSeq.eq(currentUserSeq))
+                .set(user.userNickname, userInfoRequestDto.getUserNickname())
+                .execute();
+
+        return jpaQueryFactory.selectFrom(user)
+                .where(user.userSeq.eq(currentUserSeq))
+                .fetchOne();
+    }
+
 }
