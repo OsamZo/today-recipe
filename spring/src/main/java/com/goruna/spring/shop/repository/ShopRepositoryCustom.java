74a54e75f5dd310472df75e54ebdaf6a0ea7475b
hyomee2 <s5763305@naver.com>
2024-11-06 23:49:46 +0900

    [FEAT] 오늘의 특가 리스트 조회 기능 구현

--- spring/src/main/java/com/goruna/spring/shop/repository/ShopRepositoryCustom.java
+++ spring/src/main/java/com/goruna/spring/shop/repository/ShopRepositoryCustom.java
@@ -3,9 +3,12 @@ package com.goruna.spring.shop.repository;
 import com.goruna.spring.product.entity.Product;
 import org.springframework.data.domain.Pageable;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface ShopRepositoryCustom {
+
+    List<Product> readLatest5ShopsToday(LocalDateTime startOfDay, LocalDateTime endOfDay);
 
     List<Product> readShopByCategorySeq(Long categorySeq, Pageable pageable);
 
--- spring/src/main/java/com/goruna/spring/shop/repository/ShopRepositoryImpl.java
+++ spring/src/main/java/com/goruna/spring/shop/repository/ShopRepositoryImpl.java
@@ -2,11 +2,17 @@ package com.goruna.spring.shop.repository;
 
 import com.goruna.spring.common.aggregate.YnType;
 import com.goruna.spring.product.entity.Product;
+import com.goruna.spring.shop.entity.Shop;
+import com.querydsl.core.types.SubQueryExpression;
+import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 
 import static com.goruna.spring.book.entity.QBook.book;
@@ -20,6 +26,35 @@ import static com.goruna.spring.shop.entity.QShopCategory.shopCategory;
 public class ShopRepositoryImpl implements ShopRepositoryCustom {
 
     private final JPAQueryFactory jpaQueryFactory;
+
+    // 오늘의 특가 리스트 조회
+    @Override
+    public List<Product> readLatest5ShopsToday(LocalDateTime startOfDay, LocalDateTime endOfDay) {
+        // 각 shopSeq별로 오늘 등록된 것 중 가장 최신 productSeq를 가져오는 서브쿼리
+        SubQueryExpression<Long> latestProductSubquery = JPAExpressions
+                .select(product.productSeq.max())
+                .from(product)
+                .where(product.regDate.between(startOfDay, endOfDay))
+                .groupBy(product.shop.shopSeq)
+                .orderBy(product.regDate.desc())
+                .limit(1);
+
+        // 오늘 올라온 상품 중 가장 마지막에 올라온 상품을 shop과 shopCategory와 조인
+        return jpaQueryFactory
+                .selectFrom(product)
+                .join(product.shop, shop).fetchJoin()
+                .join(shop.shopCategory, shopCategory).fetchJoin()
+                .where(product.productSeq.in(latestProductSubquery))
+                .where(shop.shopApprStatus.eq(YnType.Y))
+                .where(shop.shopDelStatus.eq(YnType.N))
+                .orderBy(product.regDate.desc())
+                .limit(5)
+                .fetch();
+
+
+
+
+    }
 
     // 카테고리별 매장 리스트 조회
     @Override
@@ -36,6 +71,7 @@ public class ShopRepositoryImpl implements ShopRepositoryCustom {
                 .fetch();
     }
 
+    // 매장별 리뷰 개수 카운트
     @Override
     public Long countReviewsByShopSeq(Long shopSeq){
         return jpaQueryFactory
