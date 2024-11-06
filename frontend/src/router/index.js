fb6aecb48698f5cfc14c36f4e83ca94f49ecfab9
minju <kminju0224@gmail.com>
2024-11-07 02:14:14 +0900

    [FEAT] router 설정

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import MainRouter from "@/router/MainRouter.js";
 import ReviewRouter from "@/router/ReviewRouter.js";
+import UserRouter from "@/router/UserRouter.js";
 
 const routes = [
     ...MainRouter,
-    ...ReviewRouter
+    ...ReviewRouter,
+    ...UserRouter
 ]
 
 const router = createRouter({
