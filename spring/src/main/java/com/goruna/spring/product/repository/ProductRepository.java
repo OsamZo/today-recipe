27762dcd658436033eb70ba064f59c8bd06b3c68
wildcat222 <wildcat222@naver.com>
2024-11-07 00:32:16 +0900

    chore: 사용하지 않는 import 삭제

--- spring/src/main/java/com/goruna/spring/product/repository/ProductRepository.java
+++ spring/src/main/java/com/goruna/spring/product/repository/ProductRepository.java
@@ -1,10 +1,7 @@
 package com.goruna.spring.product.repository;
 
-import com.goruna.spring.book.entity.Book;
 import com.goruna.spring.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
