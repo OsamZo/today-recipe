cbae3d346ce0becc213188db2e5265819e9e384c
wildcat222 <wildcat222@naver.com>
2024-11-07 00:39:52 +0900

    chore: 구조에 맞지않는 Repository 인터페이스 삭제

--- spring/src/main/java/com/goruna/spring/shop/repository/OwnerShopRepository.java
+++
@@ -1,8 +0,0 @@
-package com.goruna.spring.shop.repository;
-
-import com.goruna.spring.shop.entity.Shop;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OwnerShopRepository extends JpaRepository<Shop, Long> {
-
-}
