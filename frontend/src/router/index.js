94824e31cbf059f2c5cc0e1a177a8283f29563f9
hyomee2 <s5763305@naver.com>
2024-11-06 12:02:17 +0900

    [FEAT] 라우터 추가

---
+++ frontend/src/router/index.js
@@ -0,0 +1,22 @@
+import MainRouter from "@/router/MainRouter.js";
+
+const routes = [
+    ...MainRouter
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+    // 라우팅 시 화면 최상단으로 이동됨.
+    scrollBehavior(to, from, savedPosition) {
+        // savedPosition이 있는 경우(예: 뒤로 가기), 해당 위치로 이동
+        if (savedPosition) {
+            return savedPosition;
+        } else {
+            // 새로운 페이지 이동 시 맨 위로 스크롤
+            return { top: 0 };
+        }
+    }
+});
+
+export default router;
