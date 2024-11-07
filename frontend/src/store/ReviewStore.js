f7f7f382eb6829c2e09d73498633e85fa51b89f4
kookong2 <hy010309@naver.com>
2024-11-07 21:03:03 +0900

    [REFACTOR] ReviewStore 수정

--- frontend/src/store/ReviewStore.js
+++ frontend/src/store/ReviewStore.js
@@ -1,18 +1,20 @@
 import { defineStore } from 'pinia';
-import axios from 'axios';
+import { fetchReviewsByShopSeq } from '@/api/review/ReviewReadApi';
 
 export const useReviewStore = defineStore('reviewStore', {
     state: () => ({
         reviews: [],
     }),
     actions: {
-        async fetchReviews(shopSeq) {
-        try {
-            const response = await axios.get(`/api/v1/shop/${shopSeq}/review`);
-            this.reviews = response.data.data;
-        } catch (error) {
-            console.error('리뷰 데이터를 가져오는 중 오류가 발생했습니다:', error);
+        async loadReviews(shopSeq) {
+            try {
+                const response = await fetchReviewsByShopSeq(shopSeq);
+                console.log('API response data:', response.data);
+                this.reviews = response;
+                console.log('Reviews after setting in store:', this.reviews);
+            } catch (error) {
+                console.error('스토어에서 리뷰 데이터를 로드하는 중 오류 발생:', error);
+            }
         }
-        },
     },
 });
