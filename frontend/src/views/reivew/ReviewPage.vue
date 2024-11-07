a0e174925f01469f68edba2bf21835c039291ad1
kookong2 <hy010309@naver.com>
2024-11-07 21:03:18 +0900

    [FEAT] ReviewPage 데이터 가져오는 코드 추가

--- frontend/src/views/reivew/ReviewPage.vue
+++ frontend/src/views/reivew/ReviewPage.vue
@@ -1,16 +1,21 @@
 <template>
   <ReviewPageContent>
-    <ShopCard>
+    <ShopCard
+      :shopImgUrl="shopData.shopImgUrl"
+      :shopName="shopData.shopName"
+    >
       <template v-slot:action>
-        <button class="review-button">+ 리뷰 작성</button> <!-- 슬롯에 버튼 추가 -->
+        <button class="review-button">리뷰 작성</button>
       </template>
     </ShopCard>
     <div class="separator"></div>
-    <ReviewList />
+    <ReviewList :reviews="reviews" />
   </ReviewPageContent>
 </template>
 
 <script>
+import { useReviewStore } from '@/store/ReviewStore';
+import { onMounted, computed } from 'vue';
 import ShopCard from '@/components/ShopCard.vue';
 import ReviewList from '@/components/ReviewList.vue';
 import ReviewPageContent from '@/components/WhiteContentBox.vue';
@@ -20,6 +25,35 @@ export default {
     ShopCard,
     ReviewList,
     ReviewPageContent
+  },
+  setup() {
+    const reviewStore = useReviewStore();
+
+    onMounted(() => {
+      reviewStore.loadReviews(1); // shopSeq 값이 올바른지 확인
+      console.log('Reviews after loading:', reviews.value); // 데이터 확인
+    });
+
+    // 스토어에서 데이터를 가져오기
+    const reviews = computed(() => reviewStore.reviews);
+    const shopData = computed(() => {
+      if (reviews.value.length > 0) {
+        // 첫 번째 리뷰에서 가게 정보를 가져온다고 가정
+        return {
+          shopImgUrl: reviews.value[0].shopImgUrl,
+          shopName: reviews.value[0].shopName
+        };
+      }
+      return {
+        shopImgUrl: '',
+        shopName: ''
+      };
+    });
+
+    return {
+      reviews,
+      shopData
+    };
   }
 };
 </script>
