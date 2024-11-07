2450ed4f50ae7f0945ff5e536389a416706d5e05
kookong2 <hy010309@naver.com>
2024-11-08 02:03:40 +0900

    [FEAT] GoodDeleteResDTO 추가

---
+++ spring/src/main/java/com/goruna/spring/review/dto/GoodDeleteResDTO.java
@@ -0,0 +1,14 @@
+package com.goruna.spring.review.dto;
+
+import com.goruna.spring.common.aggregate.YnType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class GoodDeleteResDTO {
+
+    private YnType isClicked;
+}
