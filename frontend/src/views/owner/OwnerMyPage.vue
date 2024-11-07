3f83ed998023bf9ae5d27e5a326b168f42862a95
kookong2 <hy010309@naver.com>
2024-11-07 21:49:01 +0900

    [REFACTOR] 폴더 이름 수정으로 인한 경로 변경

--- frontend/src/views/owner/OwnerMyPage.vue
+++ frontend/src/views/owner/OwnerMyPage.vue
@@ -2,7 +2,7 @@
 import '@/assets/css/reset.css';
 import UserInfo from "@/views/user/UserInfo.vue";
 import {ref} from "vue";
-import ReviewPage from "@/views/reivew/ReviewReadPage.vue";
+import ReviewPage from "@/views/review/ReviewReadPage.vue";
 import OwnerBookList from "@/views/owner/OwnerBookList.vue";
 
 const selectedMenu = ref('userInfo');
---
+++ frontend/src/views/review/ReviewCreatePage.vue
@@ -0,0 +1,19 @@
+<template>
+    <ReviewPageContent>
+
+    </ReviewPageContent>
+</template>
+
+<script>
+import ReviewPageContent from '@/components/WhiteContentBox.vue';
+
+export default{
+    components: {
+        ReviewPageContent
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
