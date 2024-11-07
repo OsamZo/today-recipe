473d12305a26bf0cfd2b008692ea1c604dc79a26
kookong2 <hy010309@naver.com>
2024-11-08 02:04:07 +0900

    [REFACTOR] api 로직 수정

--- spring/src/main/java/com/goruna/spring/review/controller/GoodController.java
+++ spring/src/main/java/com/goruna/spring/review/controller/GoodController.java
@@ -2,13 +2,13 @@ package com.goruna.spring.review.controller;
 
 import com.goruna.spring.common.response.ApiResponse;
 import com.goruna.spring.common.response.ResponseUtil;
+import com.goruna.spring.review.dto.GoodDeleteResDTO;
 import com.goruna.spring.review.dto.GoodRequestDTO;
-import com.goruna.spring.review.dto.GoodResponseDTO;
+import com.goruna.spring.review.dto.GoodCreateResDTO;
 import com.goruna.spring.review.service.GoodService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -23,7 +23,7 @@ public class GoodController {
     @PostMapping("/good")
     @Operation(summary = "좋아요 추가 API", description = "좋아요 추가를 위한 API 입니다.")
     public ApiResponse<?> addGood(@PathVariable Long shopSeq, @RequestBody GoodRequestDTO requestDTO) {
-        GoodResponseDTO responseDTO = goodService.createGood(requestDTO);
+        GoodCreateResDTO responseDTO = goodService.createGood(requestDTO);
         return ResponseUtil.successResponse("좋아요가 성공적으로 추가되었습니다.", responseDTO).getBody();
     }
 
@@ -31,7 +31,7 @@ public class GoodController {
     @DeleteMapping("/good/{goodSeq}")
     @Operation(summary = "좋아요 삭제 API", description = "좋아요 삭제를 위한 API 입니다.")
     public ApiResponse<?> deleteGood(@PathVariable Long shopSeq, @PathVariable Long goodSeq) {
-        goodService.deleteGood(goodSeq);
+        GoodDeleteResDTO goodDeleteResDTO = goodService.deleteGood(goodSeq);
         return ResponseUtil.successResponse("좋아요가 성공적으로 삭제되었습니다.", null).getBody();
     }
 }
