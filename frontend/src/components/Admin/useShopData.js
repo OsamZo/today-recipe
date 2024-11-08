34e4f3b99248fc2419b257115a08af0c0025e212
WhooGeek <rlarjswn0303@naver.com>
2024-11-08 17:48:27 +0900

    [FEAT] 가게 사업자 정보 상세조회 useShopDate.js 파일 분리 추가

---
+++ frontend/src/components/Admin/useShopData.js
@@ -0,0 +1,24 @@
+// composables/useShopData.js
+import { ref } from 'vue';
+import axios from 'axios';
+
+export function useShopData(shopSeq) {
+    const shop = ref({});
+
+    const fetchShops = async () => {
+        try {
+            const response = await axios.get(`http://localhost:8100/api/v1/admin/shop/${shopSeq}/auth`);
+            shop.value = response.data.data;
+            console.log(shop.value);
+        } catch (error) {
+            console.error("데이터를 불러오는 중 오류 발생:", error);
+        }
+    };
+
+    
+
+    return {
+        shop,
+        fetchShops
+    };
+}
