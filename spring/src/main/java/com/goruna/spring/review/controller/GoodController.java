b8be4526593589aef0a9e85fcddb2372729fecab
kookong2 <hy010309@naver.com>
2024-11-10 00:02:49 +0900

    [FEAT] 좋아요 상태 조회 컨트롤러 추가

--- spring/src/main/java/com/goruna/spring/review/controller/GoodController.java
+++ spring/src/main/java/com/goruna/spring/review/controller/GoodController.java
@@ -2,14 +2,14 @@ package com.goruna.spring.review.controller;
 
 import com.goruna.spring.common.response.ApiResponse;
 import com.goruna.spring.common.response.ResponseUtil;
-import com.goruna.spring.review.dto.GoodDeleteResDTO;
 import com.goruna.spring.review.dto.GoodRequestDTO;
-import com.goruna.spring.review.dto.GoodCreateResDTO;
+import com.goruna.spring.review.dto.GoodResDTO;
 import com.goruna.spring.review.service.GoodService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/api/v1/shop/{shopSeq}/review")
@@ -19,19 +19,29 @@ public class GoodController {
 
     private final GoodService goodService;
 
+    /* 좋아요 상태 조회 */
+    @GetMapping("/good")
+    @Operation(summary = "좋아요 상태 조회 API", description = "좋아요 상태 조회 위한 API 입니다.")
+    public ApiResponse<?> readGood(@PathVariable Long shopSeq, @RequestParam Long userSeq, @RequestParam Long reviewSeq) {
+        GoodResDTO responseDTO = goodService.readGood(userSeq, reviewSeq);
+        return ResponseUtil.successResponse("좋아요 조회 성공", responseDTO).getBody();
+    }
+
     /* 좋아요 추가 */
     @PostMapping("/good")
     @Operation(summary = "좋아요 추가 API", description = "좋아요 추가를 위한 API 입니다.")
     public ApiResponse<?> addGood(@PathVariable Long shopSeq, @RequestBody GoodRequestDTO requestDTO) {
-        GoodCreateResDTO responseDTO = goodService.createGood(requestDTO);
-        return ResponseUtil.successResponse("좋아요가 성공적으로 추가되었습니다.", responseDTO).getBody();
+
+        goodService.createGood(requestDTO);
+        return ResponseUtil.successResponse("좋아요가 성공적으로 추가되었습니다.").getBody();
     }
 
     /* 좋아요 삭제 */
     @DeleteMapping("/good/{goodSeq}")
     @Operation(summary = "좋아요 삭제 API", description = "좋아요 삭제를 위한 API 입니다.")
     public ApiResponse<?> deleteGood(@PathVariable Long shopSeq, @PathVariable Long goodSeq) {
-        GoodDeleteResDTO goodDeleteResDTO = goodService.deleteGood(goodSeq);
-        return ResponseUtil.successResponse("좋아요가 성공적으로 삭제되었습니다.", null).getBody();
+
+        goodService.deleteGood(goodSeq);
+        return ResponseUtil.successResponse("좋아요가 성공적으로 삭제되었습니다.").getBody();
     }
 }
