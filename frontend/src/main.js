478c387855df3174ea8545e9fbbf6a62e097a629
WhooGeek <rlarjswn0303@naver.com>
2024-11-09 18:31:10 +0900

    [REFACTOR] 피니아 use, 플러그인 import

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,12 +7,14 @@ import {library} from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import {faCalendar} from "@fortawesome/free-regular-svg-icons/faCalendar";
 import { faBookmark } from '@fortawesome/free-solid-svg-icons';
+import piniaPersist from 'pinia-plugin-persistedstate';
 
 library.add(faCalendar);
 library.add(faBookmark);
 
 const app = createApp(App);
 const pinia = createPinia();
+pinia.use(piniaPersist);
 
 app.use(router);
 app.component('font-awesome-icon', FontAwesomeIcon);
