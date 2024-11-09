ce95fdda2650d52cf01e246b5fc11096f0415023
kookong2 <hy010309@naver.com>
2024-11-10 00:01:44 +0900

    [FEAT] 좋아요 조회 repository 메서드 추가

--- spring/src/main/java/com/goruna/spring/review/repository/GoodRepository.java
+++ spring/src/main/java/com/goruna/spring/review/repository/GoodRepository.java
@@ -1,7 +1,12 @@
 package com.goruna.spring.review.repository;
 
 import com.goruna.spring.review.entity.Good;
+import com.goruna.spring.review.entity.Review;
+import com.goruna.spring.users.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface GoodRepository extends JpaRepository<Good, Long> {
+    Optional<Good> findByUserAndReview(User user, Review review);
 }
