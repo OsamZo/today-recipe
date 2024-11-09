150b36b8ac03ea97eeeea62d59b449ad4aafe09d
wildcat222 <wildcat222@naver.com>
2024-11-09 15:54:15 +0900

    feat: (사장) 마이페이지 매장 정보 조회 api 연동

---
+++ frontend/src/store/ShopStore.js
@@ -0,0 +1,29 @@
+import {defineStore} from "pinia";
+import {fetchOwnerShopInfo} from "@/api/shop/ShopReadApi.js";
+
+export const ShopStore = defineStore('ShopStore', {
+    state: () => ({
+        shopData: JSON.parse(localStorage.getItem('shopData')) || {
+            userSeq: '',
+            shopSeq: '',
+            shopName: '',
+            shopAddress: '',
+            shopIntroduction: ''
+        }
+    }),
+    actions: {
+        async loadOnwerShopData(shopSeq){
+            try {
+                const shopInfo = await fetchOwnerShopInfo();
+                this.shopData = shopInfo;
+
+                // 로컬 스토리지에 저장
+                localStorage.setItem('shopData', JSON.stringify(this.shopData));
+            } catch (error) {
+                console.log('스토어에서 매장 데이터를 로드하는 중 오류 발생 :', error);
+            }
+        }
+
+
+    }
+});
