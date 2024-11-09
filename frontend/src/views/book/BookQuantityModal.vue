f0f40757530983bfa3dcdab2847d32b35490db53
hyomee2 <s5763305@naver.com>
2024-11-09 19:03:25 +0900

    [FEAT] 예약 수량 모달 생성 및 예약하기 백엔드 연동

---
+++ frontend/src/views/book/BookQuantityModal.vue
@@ -0,0 +1,123 @@
+<script setup>
+import {ref} from "vue";
+import axios from "axios";
+
+const props = defineProps({
+  productQty: {
+    type: Number,
+    required: true
+  },
+  productSeq: {
+    type: Number,
+    required: true
+  },
+});
+
+// 수량 조절
+const selectedQuantity = ref(1);
+const updateQuantity = (change) => {
+  const updatedQuantity = selectedQuantity.value + change;
+  if (updatedQuantity < 1) {
+    alert("1개 이상부터 예약 가능합니다.")
+    return;
+  }
+  if (updatedQuantity > props.productQty) {
+    return;
+  }
+  selectedQuantity.value = updatedQuantity;
+}
+
+const booking = async() => {
+  try {
+    console.log(props.productSeq)
+    const response = await axios.post(`http://localhost:8100/api/v1/product/${props.productSeq}`, null, {
+      params: {
+        userSeq: 1,
+        bookQty: selectedQuantity.value
+      }
+    });
+    alert("예약이 완료되었습니다.");
+  } catch(error) {
+    alert("예약에 실패했습니다.");
+  }
+}
+</script>
+
+<template>
+  <div class="modal-box">
+    <div class="modal-content-box flex">
+      <div class="text-box">
+        <div class="title-text">구매하시려는 개수를 입력하세요.</div>
+        <div class="available-quantity flex">현재 남은 개수: {{ productQty }}개</div>
+      </div>
+      <div class="flex quantity-box">
+        <div>
+          <img class="quantity-button-img" @click="updateQuantity(-1)" src="https://goruna.s3.us-west-1.amazonaws.com/6e63033f-4c2e-4028-9f91-f73f1790c465_minus.png" alt="수량 감소 버튼">
+        </div>
+        <div class="selected-quantity">{{ selectedQuantity }}</div>
+        <div>
+          <img class="quantity-button-img" @click="updateQuantity(1)"src="https://goruna.s3.us-west-1.amazonaws.com/8bee0745-157d-42e2-8610-745c98c30cf5_img.png" alt="수량 증가 버튼">
+        </div>
+      </div>
+      <button class="book-button" @click="booking()">예약하기</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.flex {
+  display: flex;
+}
+
+.modal-box {
+  width: 566px;
+  height: 328px;
+  background-color: var(--white-background);
+  padding: 87px 0 0 0;
+}
+
+.modal-content-box {
+  width: 418px;
+  margin: 0 auto;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.title-text {
+  font-size: 30px;
+}
+
+.available-quantity {
+  justify-content: center;
+  margin: 10px auto;
+  font-size: 15px;
+  color: var(--text-red);
+}
+
+.quantity-box {
+  margin: 33px 0;
+  align-items: center;
+}
+
+.quantity-button-img {
+  width: 24px;
+  height: 24px;
+  cursor: pointer;
+}
+
+.selected-quantity {
+  margin: 0 37px;
+}
+
+.book-button {
+  font-family: "Gowun Dodum";
+  width: 120px;
+  height: 29px;
+  background-color: var(--button-brown);
+  border: none;
+  border-radius: 10px;
+  color: var(--text-white);
+  cursor: pointer;
+}
+</style>
