c22d9477365a2e56d8a3fa6007f36687250f53fa
wildcat222 <wildcat222@naver.com>
2024-11-11 01:43:40 +0900

    feat: 중간 컴포넌트인 ShopInfo의 렌더링 조건 설정 및 데이터 전달 및 이벤트 수신

--- frontend/src/views/owner/OwnerMyPage.vue
+++ frontend/src/views/owner/OwnerMyPage.vue
@@ -11,14 +11,20 @@ import {useShopStore} from "@/store/ShopStore.js";
 // 메뉴 선택 시 component 전환
 const selectedMenu = ref('userInfo');
 const shopStore = useShopStore();
+const propsComponent = ref('');
+
+const isComponent = (component) => {
+  propsComponent.value = component;
+}
+
 
 
 const shopData = computed(() => shopStore.shopData);
-const selectMenu = (menu) => {
+const selectMenu = async (menu) => {
   console.log(menu);
   selectedMenu.value = menu;
   if(selectedMenu.value === 'ShopInfo') {
-    shopStore.loadOnwerShopData();
+    await shopStore.loadOnwerShopData();
   }
 };
 
@@ -87,7 +93,13 @@ const selectMenu = (menu) => {
           <UserInfo v-if="selectedMenu === 'userInfo'"/>
           <ReviewPage v-else-if="selectedMenu === 'ReviewPage'"/>
           <OwnerBookList v-else-if="selectedMenu === 'OwnerBookList'"/>
-          <ShopInfo v-else-if="selectedMenu === 'ShopInfo'" :shopData="shopData"/>
+          <ShopInfo
+              v-else-if="selectedMenu === 'ShopInfo'"
+              :shopData="shopData"
+              :propsComponent="propsComponent"
+              @update-shopInfo="updateShopData"
+              @update-product="selectMenu('OwnerProduct')"
+              @delete-shop="deleteShop"/>
           <OwnerProduct v-else-if="selectedMenu === 'OwnerProduct'"/>
           <!-- 다른 컴포넌트들 추가해서 사용하세요 -->
         </div>
