f1f19122b8704d0e20c9576c5569e5abae51ffa3
wildcat222 <wildcat222@naver.com>
2024-11-11 01:41:48 +0900

    feat: (사장) 매장 정보 수정, 삭제 api 연동

---
+++ frontend/src/api/shop/shopApi.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+// (사장) 매장 정보 수정 API (const updateShopData = reactive({});
+export const updateOwnerShop = async (shopSeq, updateShopData) => {
+    try {
+        // console.log(shopSeq, updateShopData.value);
+        await axios.put(`http://localhost:8100/api/v1/owner/shop/${shopSeq}`, updateShopData.value
+        );
+    } catch (error) {
+        console.error('매장 정보를 수정하는 중 오류가 발생했습니다:', error);
+        throw error;
+    }
+}
+
+// (사장) 매장 정보 삭제 API
+export const deleteOwnerShop = async (shopSeq) => {
+    try {
+        await axios.delete(`http://localhost:8100/api/v1/owner/shop/${shopSeq}`);
+    } catch (error) {
+        console.error('매장 정보를 삭제하는 중 오류가 발생했습니다:', error);
+        throw error;
+    }
+
+}
