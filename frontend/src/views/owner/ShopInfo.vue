1a48e3685392eb83ca10e0e7a16052ea68bc3837
wildcat222 <wildcat222@naver.com>
2024-11-11 01:41:22 +0900

    feat: ShowInfo 조건부 렌더링을 위한 기능별 컴포넌트 분리 및 중간 컴포넌트 역할인 이벤트, 입력값 전달 기능 구현

--- frontend/src/views/owner/ShopInfo.vue
+++ frontend/src/views/owner/ShopInfo.vue
@@ -1,12 +1,50 @@
 <script setup>
 import '@/assets/css/reset.css';
+import ReadShop from "@/views/owner/ReadShop.vue";
+import UpdateShop from "@/views/owner/UpdateShop.vue";
+import {ref} from "vue";
 
-defineProps({
+const props = defineProps({
   shopData: {
     type: Object,
     required: true
+  },
+  propsComponent: {
+    type: String,
+    required: true
   }
+
 });
+
+const emit = defineEmits(['update-product', 'update-shopInfo']);
+
+const forwardData = () => {
+  emit('update-product');
+};
+
+const receivedShopData = ref({
+  shopTel: '',
+  shopAddress: '',
+  shopIntroduction: ''
+});
+const forwardShopData = async (inputShopData) => {
+  receivedShopData.value = inputShopData;
+  // console.log(receivedShopData.value.shopTel);
+  await emit('update-shopInfo', receivedShopData);
+  console.log(props.propsComponent);
+  showComponent(props.propsComponent);
+};
+
+const forwardEvent = () => {
+  emit('delete-shop');
+}
+
+const showedComponent = ref('readShop');
+
+const showComponent = (component) => {
+  // console.log(component);
+  showedComponent.value = component;
+};
 
 </script>
 
@@ -14,46 +52,44 @@ defineProps({
   <div class="total-content">
     <div class="page-content-title">
       <div class="menu-title">매장 정보</div>
+      <button
+          class="update"
+          @click="showComponent('updateShop')">수정하기</button>
     </div>
 
-    <div class="content-container">
+    <ReadShop
+        v-if="showedComponent === 'readShop'"
+        @update-product="forwardData"
+        @delete-shop="forwardEvent"
+        :shopData="props.shopData"
+    />
+    <UpdateShop
+        v-if="showedComponent === 'updateShop'"
+        @update-shopInfo="forwardShopData"
+        @canceled-update="showComponent('readShop')"
+        :shopData="props.shopData"
+    />
 
-      <div class="inline">
-        <div class="column-name">매장명</div>
-        <div class="inline">
-          <div class="column-content" style="margin-right: 30px">{{ shopData.shopName }}</div>
-        </div>
-      </div>
-
-      <div class="inline">
-        <div class="column-name">주소</div>
-        <div class="column-content">{{ shopData.shopAddress}}</div>
-      </div>
-
-      <div class="inline">
-        <div class="column-name">매장 소개</div>
-        <div class="column-content">{{ shopData.shopIntroduction }}</div>
-      </div>
-
-
-      <br><br><br><br>
-
-      <div class="inline">
-        <button class="update-shopinfo" @click="">수정하기</button>
-        <button class="delete-shopinfo" @click="">매장 삭제</button>
-      </div>
-
-    </div>
   </div>
+
 </template>
 
 <style scoped>
-.content-container{
-  display: flex;
-  flex-direction: column;
-  margin-left: 30%;
+.total-content {
+  width: 100%;
 }
-.update-shopinfo{
+.page-content-title {
+  display: flex;
+  padding-bottom: 12px;
+  border-bottom: 1px solid var(--button-brown);
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 130px;
+}
+.menu-title {
+  font-size: 30px;
+}
+.update {
   margin-right: 10px;
   width: 116px;
   height: 42px;
@@ -65,46 +101,6 @@ defineProps({
   align-items: center;
   border: none;
   cursor: pointer;
-}
-.delete-shopinfo{
-  margin-right: 10px;
-  width: 116px;
-  height: 42px;
-  color: var(--text-black);
-  border-radius: 10px;
-  background: var(--button-brown2);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border: none;
-  cursor: pointer;
-}
-.column-content{
-  font-size: 30px;
-}
-.column-name{
-  font-size: 28px;
-  color: var(--text-black);
-  width: 230px;
-}
-.inline{
-  display: flex;
-  align-items: center;
-  margin-bottom: 10px;
-}
-.total-content{
-  width: 100%;
-}
-.page-content-title{
-  display: flex;
-  padding-bottom: 12px;
-  border-bottom: 1px solid var(--button-brown);
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 130px;
-}
-.menu-title{
-  font-size: 30px;
 }
 
 </style>
