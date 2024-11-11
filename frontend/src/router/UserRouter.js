323bc81d4ba5e87a8a44f01308770ff98c5b4d5f
WhooGeek <rlarjswn0303@naver.com>
2024-11-11 12:46:52 +0900

    [FEAT] 내가 쓴 댓글 조회 라우팅 설정

--- frontend/src/router/UserRouter.js
+++ frontend/src/router/UserRouter.js
@@ -2,6 +2,7 @@ import UserMyPage from "@/views/user/UserMyPage.vue";
 import LoginPage from "@/views/user/LoginPage.vue";
 import LoginUserNickName from "@/views/user/LoginUserNickName.vue";
 import MyPageBook from "@/views/user/MyPageBook.vue";
+import MyPageReview from "@/views/user/MyPageReview.vue";
 
 export default [
     {
@@ -19,5 +20,9 @@ export default [
     {
         path: '/book',
         component: MyPageBook
+    },
+    {
+        path: '/user/review',
+        component: MyPageReview
     }
 ];
