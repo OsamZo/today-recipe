9e24ea8fb5581a40f1b80d09710ea4c101a96a9d
kookong2 <hy010309@naver.com>
2024-11-12 10:15:35 +0900

    [REFACTOR] 로그인 페이지 화면 비율 조정

--- frontend/src/views/user/LoginPage.vue
+++ frontend/src/views/user/LoginPage.vue
@@ -23,44 +23,59 @@ const initiateGoogleLogin = () => {
   <div class="content-box">
     <div class="login-title">SNS 계정으로 간편 로그인</div>
     <div class="gray-btn">
-      <img src="https://goruna.s3.us-west-1.amazonaws.com/1a90a548-a272-4c5d-aa3f-076c6b74997d_kakaotalk.png">
+      <img src="https://goruna.s3.us-west-1.amazonaws.com/1a90a548-a272-4c5d-aa3f-076c6b74997d_kakaotalk.png" alt="Kakao Login">
       <div class="login-type">카카오 계정으로 로그인</div>
     </div>
     <div class="gray-btn" @click="initiateGoogleLogin">
-      <img src="https://goruna.s3.us-west-1.amazonaws.com/755c83be-a394-4344-801c-6b3f8f118b0f_google.png">
+      <img src="https://goruna.s3.us-west-1.amazonaws.com/755c83be-a394-4344-801c-6b3f8f118b0f_google.png" alt="Google Login">
       <div class="login-type">구글 계정으로 로그인</div>
     </div>
   </div>
 </template>
 
 <style scoped>
-.login-type{
+.login-type {
   margin: auto;
+  font-size: 1rem;
 }
-.login-title{
-  font-size: 25px;
-  margin-bottom: 50px;
+
+.login-title {
+  font-size: 2rem;
+  margin-bottom: 3rem;
   font-weight: bold;
+  text-align: center;
 }
-.content-box{
+
+.content-box {
   border-radius: 43px;
   background: #FFF;
   box-shadow: 0px 4px 13px 0px rgba(0, 0, 0, 0.13) inset;
-  padding: 100px 130px;
-  margin: 100px 300px;
+  padding: 5vw 5vh;
+  margin: 5vw auto;
   align-items: center;
   display: flex;
   flex-direction: column;
+  max-width: 800px;
+  min-width: 300px;
+  width: 80%; /* 비율 유지 */
 }
-.gray-btn{
+
+.gray-btn {
   display: flex;
   background-color: var(--button-gray);
   border-radius: 52px;
   align-items: center;
-  width: 40%;
-  height: 44px;
+  width: 100%; /* 부모 요소의 너비에 맞춰 조정 */
+  max-width: 400px;
+  min-width: 150px;
+  height: 3rem;
   justify-content: flex-start;
-  padding: 0px 20px;
-  margin: 5px 0px;
+  padding: 0 1rem;
+  margin: 0.5rem 0;
+}
+
+.gray-btn img {
+  height: 2rem;
+  margin-right: 0.5rem;
 }
 </style>
