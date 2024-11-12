ab66baae1303855d9b53f5b065b87df041b76cc7
kookong2 <hy010309@naver.com>
2024-11-12 10:14:39 +0900

    [FEAT] 로그아웃 api 추가

--- frontend/src/api/user/UserApi.js
+++ frontend/src/api/user/UserApi.js
@@ -36,7 +36,7 @@ export const submitNickname = async (userSeq, nickname) => {
 };
 
 // 마이페이지 예약 내역 조회
-export const fetchBookList = async(userSeq) => {
+export const fetchBookList = async (userSeq) => {
   try {
     const response = await axios.get(`http://localhost:8100/api/v1/user/${userSeq}/book`);
     return response.data.data;
@@ -44,10 +44,10 @@ export const fetchBookList = async(userSeq) => {
     console.log("예약 내역 조회 중 오류 발생", error);
     return [];
   }
-}
+};
 
 // 회원 정보 조회
-export const fetchUserInfo = async(userSeq) => {
+export const fetchUserInfo = async (userSeq) => {
   try {
     const response = await axios.get(`http://localhost:8100/api/v1/user/${userSeq}/info`);
     return response.data.data;
@@ -55,40 +55,53 @@ export const fetchUserInfo = async(userSeq) => {
     console.log("회원 정보 조회 중 오류", error);
     return [];
   }
-}
+};
 
 // 회원 이용 내역 조회
-export const fetchUserHistory = async(userSeq) => {
+export const fetchUserHistory = async (userSeq) => {
   try {
     const response = await axios.get(`http://localhost:8100/api/v1/user/${userSeq}/history`);
     return response.data.data;
-  } catch(error) {
+  } catch (error) {
     console.log("회원 이용 내역 조회 중 오류", error);
     return [];
   }
-}
+};
 
 // 회원 정보 수정
-export const updateUserInfo = async(userSeq, userNickname) => {
+export const updateUserInfo = async (userSeq, userNickname) => {
   try {
     console.log(userNickname);
     const response = await axios.put(`http://localhost:8100/api/v1/user/${userSeq}/info`, {
       userNickname,
     });
     return response.data;
-  } catch(error) {
+  } catch (error) {
     console.log("회원 정보 수정 중 오류 발생", error);
     return [];
   }
-}
+};
 
 // 회원 탈퇴
-export const deleteUser = async(userSeq) => {
+export const deleteUser = async (userSeq) => {
   try {
     const response = await axios.delete(`http://localhost:8100/api/v1/user/${userSeq}`);
     return response.data;
-  } catch(error){
+  } catch (error) {
     console.log("회원 탈퇴 도중 오류 발생", error);
     return [];
   }
-}
+};
+
+// 로그아웃
+export const logoutUser = async () => {
+  try {
+    const response = await axios.post('http://localhost:8100/api/v1/logout', {}, {
+      withCredentials: true // 쿠키 포함 요청
+    });
+    return response.data;
+  } catch (error) {
+    console.error('로그아웃 중 오류 발생:', error);
+    throw error;
+  }
+};
